797ed1e19a16abd11e931441992d26ee49e0f8f6
Joao Lucas Silva <joaolucassalvatore@gmail.com>
2021-08-23 21:44:10 -0300

    Alterar forma da carga inicial no banco H2

--- src/main/java/br/com/parcelaae/app/config/TestConfig.java
+++ src/main/java/br/com/parcelaae/app/config/TestConfig.java
@@ -1,23 +1,10 @@
 package br.com.parcelaae.app.config;
 
-import br.com.parcelaae.app.services.DBService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import java.text.ParseException;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
 
-    @Autowired
-    private DBService dbService;
-
-    @Bean
-    public boolean instantiateDatabase() throws ParseException {
-        dbService.instantiateTestDatabase();
-        return true;
-    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,31 @@
+insert into state (id, name) values (1, 'Pernambuco');
+insert into state (id, name) values (2, 'São Paulo');
+
+insert into city (id, name, state_id) values (1, 'Recife', 1);
+insert into city (id, name, state_id) values (2, 'Olinda', 1);
+insert into city (id, name, state_id) values (3, 'São Paulo', 2);
+
+insert into specialty (id, name) values (1, 'Clinica Geral');
+insert into specialty (id, name) values (2, 'Dermatologia');
+insert into specialty (id, name) values (3, 'Odontologia');
+
+insert into user (id, email, name, password) values (1, 'ok@doutor.com', 'Ok Doutor', '$2a$10$VdEtcZfZQbgLX9E3wykq/OX6ENEj83s2nPsQuUznBVhkJQaYiTFIW');
+insert into clinic (cnpj, id) values ('12345678910', 1);
+insert into address (id, city_id, complement, latitude, longitude, neighborhood, number, public_area, user_id, zip_code) values (1, 1, 'Apto 303', null, null, 'Espinheiro', '300', 'Av. Agamenon Magalhães', 1, '38220834');
+insert into address (id, city_id, complement, latitude, longitude, neighborhood, number, public_area, user_id, zip_code) values (2, 2, null, null, null, 'Centro', '105', 'Presidente Kennedy', 1, 38777012);
+insert into phones (user_id, phones) values (1, '34353637');
+insert into phones (user_id, phones) values (1, '81994020345');
+insert into profiles (user_id, profiles) values (1, 1);
+insert into profiles (user_id, profiles) values (1, 3);
+insert into clinic_specialties (clinic_id, specialties_id) values (1, 2);
+insert into clinic_specialties (clinic_id, specialties_id) values (1, 3);
+
+insert into user (id, email, name, password) values (2, 'sirio@libanes.com', 'Sirio Libanes', '$2a$10$/ZcXT4sPz7qiLzc2qJlpbep1EF7ZCXPmUrEtKLcE3lmxLjG6f9AY6');
+insert into clinic (cnpj, id) values ('10987654321', 2);
+insert into address (id, city_id, complement, latitude, longitude, neighborhood, number, public_area, user_id, zip_code) values (3, 3, null, null, null, 'Centro', '2106', 'Avenida Paulista', 2, 39867024);
+insert into phones (user_id, phones) values (2, '11994020345');
+insert into phones (user_id, phones) values (2, '32353937');
+insert into profiles (user_id, profiles) values (2, 3);
+insert into clinic_specialties (clinic_id, specialties_id) values (2, 1);
+insert into clinic_specialties (clinic_id, specialties_id) values (2, 2);
+insert into clinic_specialties (clinic_id, specialties_id) values (2, 3);
