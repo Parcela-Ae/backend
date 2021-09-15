0b35718c35cffed792da689f9270cf07029ea5e2
Joao Lucas Silva <joaolucassalvatore@gmail.com>
2021-09-15 20:03:52 -0300

    Adicionar endpoint /users/profile

--- src/main/java/br/com/parcelaae/app/controllers/UserController.java
+++ src/main/java/br/com/parcelaae/app/controllers/UserController.java
@@ -1,7 +1,12 @@
 package br.com.parcelaae.app.controllers;
 
+import br.com.parcelaae.app.dto.UserProfileDTO;
+import br.com.parcelaae.app.security.SecurityUtil;
+import br.com.parcelaae.app.security.UserSS;
 import br.com.parcelaae.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,11 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserProfileDTO> getUserProfile() {
+        UserSS userSS = SecurityUtil.getUserSS();
+        var dto = userService.getUserProfile(userSS);
+        return ResponseEntity.ok(dto);
+    }
 }
